27b3db42c099b2c067e3afde9e500d1b643ed6ad
wjx <820735881@qq.com>
2019-01-10 19:23:55 +0800

    itsdangers

--- mall/apps/oauth/urls.py
+++ mall/apps/oauth/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from .views import OAuthQQURLAPIView,OAuthQQUserAPIView
 urlpatterns = [
     url(r'^qq/statues/$',OAuthQQURLAPIView.as_view()),
-    url(r'qq/user/$',OAuthQQUserAPIView.as_view())
+    url(r'qq/users/$',OAuthQQUserAPIView.as_view())
 ]
--- mall/apps/oauth/views.py
+++ mall/apps/oauth/views.py
@@ -4,6 +4,9 @@ from rest_framework.views import APIView
 from QQLoginTool.QQtool import OAuthQQ
 from mall import settings
 from rest_framework import status
+
+from oauth.models import OAuthQQUser
+
 '''
 当用户点击qq 按钮的时候会 发送一个请求
 我们后端返回给他一个url（URL是根据 文档拼接出来的）
@@ -53,8 +56,58 @@ class OAuthQQUserAPIView(APIView):
         oauth = OAuthQQ(client_id=settings.QQ_CLIENT_ID,
                         client_secret=settings.QQ_CLIENT_SECRET,
                         redirect_uri=settings.QQ_REDIRECT_URI,)
-        token = OAuthQQ.get_access_token(code)
+        token = oauth.get_access_token(code)
 
         # 3.用token换openid
-        openid = OAuthQQ.get_open_id(token)
-        pass
+        openid = oauth.get_open_id(token)
+
+        #openid 是此网站上唯一对应用户身份的标识，网站可将此id进行存储
+        #便于用户下次登录 辨识其身份
+        #获取openid 的两种情况“
+        #1.用户之前绑定过
+        #2. 用户之前没有绑定过
+        #根据openid查询数据
+        try:
+            qquser = OAuthQQUser.objects.get(openid=openid)
+        except OAuthQQUser.DoesNotExist:
+            #不存在
+
+
+            return Response({'access_token':openid})
+
+        else:
+            #存在 让用户登录
+            from rest_framework_jwt.settings import api_settings
+
+            jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+            jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+            payload = jwt_payload_handler(qquser.user)
+            token = jwt_encode_handler(payload)
+
+            return Response({
+                'token':token,
+                'username':qquser.user.username,
+                'user_id':qquser.user.id
+            })
+
+
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from mall import settings
+#1.创建一个序列化器
+# secret_key 密匙
+#expires——in = None 过期时间 单位是秒
+s = Serializer(secret_key=settings.SECRET_KEY,expires_in=3600)
+
+# 组织数据
+data = {
+    'openid':'1234567890'
+}
+# 3. 让序列化其对数据进行处理
+token = s.dumps(data)
+
+#4. 获取数据对数据进行解密
+s.loads(token)
+
+# 如果token 被篡改了 是可以检测到的
+#如果token过期了 会报异常
